b2daffd48c28999ebcb5b56aa7eeb7ba507fd991
Richard ZZY <andarkge@hotmail.com>
2017-07-01 15:31:54 -0700

    Adding

---
+++ SignalR/SignalR_WebChat/MessageHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR_WebChat
+{
+    public class MessageHub : Hub
+    {
+        public void Login(string username)
+        {
+            // Notify all clients that a user is logged in
+            Clients.All.userLoggedIn(username);
+        }
+
+        public void SendMessage(string sender, string message)
+        {
+            // Display the new message by calling displayMessage on all connected clients
+            Clients.All.displayMessage(sender, message);
+        }
+
+        public void BroadCastServerTime()
+        {
+            Clients.All.MessageREceiver(DateTime.Now);
+        }
+    }
+}
